693af81ccfffd067be57c834fbae5de4715af80c
Berica98 <berica.george@gmail.com>
2024-11-26 18:45:47 +0100

    change

--- 0-making_change.py
+++ 0-making_change.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
 
 
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed
-        to meet a given amount total"""
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
     if total <= 0:
         return 0
 
-    current_total = 0
-    used_coins = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        r = (total - current_total) // coin
-        current_total += r * coin
-        used_coins += r
-        if current_total == total:
-            return used_coins
-    return -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
